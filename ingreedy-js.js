26d3a6c65597511f07f96d312f39d2a7ecfb836e
Tom White <tom.e.white@gmail.com>
2019-03-24 21:18:07 +0000

    Utils

---
+++ ingreedy-js.js
@@ -0,0 +1,17 @@
+function cleanUnicodeFractions(s) {
+  var fractions = {
+              '\u215b': '1/8',
+              '\u215c': '3/8',
+              '\u215d': '5/8',
+              '\u215e': '7/8',
+              }
+  for(var unicode in fractions) {
+    var ascii = fractions[unicode];
+    s = s.replace(unicode, ' ' + ascii)
+  }
+  return s;
+}
+
+function clumpFractions(s) {
+    return s.replace(/(\d+)\s+(\d)\/(\d)/, '$1$$$2/$3')
+}
