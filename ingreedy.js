3a3802df90c03cd44677bf8bd35d500c080fa619
Tom White <tom.e.white@gmail.com>
2019-04-21 15:10:42 +0100

    clang-format

--- ingreedy.js
+++ ingreedy.js
@@ -5749,44 +5749,44 @@ function tsvToJson(tsv) {
   return result;
 }
 var unitToGrams = {
-    "clove": 5,
-    "gram": 1,
-    "millilitre": 1,
-    "ounce": 28.3,
-    "pound": 454,
-    "teaspoon": 5,
-    "tablespoon": 15
+  'clove': 5,
+  'gram': 1,
+  'millilitre': 1,
+  'ounce': 28.3,
+  'pound': 454,
+  'teaspoon': 5,
+  'tablespoon': 15
 };
 
 var foodMeasures = {
-    "apple": 175,
-    "banana": 151,
-    "mandarin": 78,
-    "mango": 274,
-    "pear": 198,
-    "aubergine": 572,
-    "avocado": 210,
-    "bok choy": 235,
-    "capsicum": 275,
-    "carrot": 168,
-    "chilli": 15,
-    "corn": 320,
-    "cucumber": 187,
-    "lettuce": 570,
-    "mushroom": 100,
-    "onion": 130,
-    "potato": 229,
-    "tomato": 145
+  'apple': 175,
+  'banana': 151,
+  'mandarin': 78,
+  'mango': 274,
+  'pear': 198,
+  'aubergine': 572,
+  'avocado': 210,
+  'bok choy': 235,
+  'capsicum': 275,
+  'carrot': 168,
+  'chilli': 15,
+  'corn': 320,
+  'cucumber': 187,
+  'lettuce': 570,
+  'mushroom': 100,
+  'onion': 130,
+  'potato': 229,
+  'tomato': 145
 };
 
 function normalizeQuantity(quantity) {
   var match = quantity.match(/(\d+) (\d+)\/(\d+)/)
   if (match != null) {
-    return Number(match[1]) + Number(match[2])/Number(match[3])
+    return Number(match[1]) + Number(match[2]) / Number(match[3])
   }
   match = quantity.match(/(\d+)\/(\d+)/)
   if (match != null) {
-    return Number(match[1])/Number(match[2])
+    return Number(match[1]) / Number(match[2])
   }
   return Number(quantity);
 }
