ceae4453579b876062fa5553145650a4436b644c
Tom White <tom.e.white@gmail.com>
2020-04-16 18:01:05 +0100

    Look at improvement after using AFCD

---
+++ measure-success.sh
@@ -0,0 +1,4 @@
+node tools/find-outcomes.js data/ocr-responses "food not found" true
+node tools/find-outcomes.js data/ocr-responses "food not found"
+node tools/find-outcomes.js data/ingreedy-original/ingredients_gold.tsv "food not found" true
+node tools/find-outcomes.js data/ingreedy-original/ingredients_gold.tsv "food not found"
